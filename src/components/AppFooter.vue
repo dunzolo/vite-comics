eed75ed0279f6cc4a70635f427f6cba92cfd8d1c
dunzolo <rossidavide108@gmail.com>
2023-01-11 18:55:11 +0100

    added footer content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -47,6 +47,67 @@ export default {
                 </li>
             </ul>
         </div>
+        <div class="content-footer">
+            <div class="container">
+                <div class="footer-links">
+                    <div class="links">
+                        <h2>DC COMICS</h2>
+                        <nav>
+                            <ul>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                        </nav>
+                        <h2>SHOP</h2>
+                        <nav>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="links">
+                        <h2>DC</h2>
+                        <nav>
+                            <ul>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy (New)</a></li>
+                                <li><a href="#">Ad Chois</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshop</a></li>
+                                <li><a href="#">CPSC Cerification</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contacts Us</a></li>
+    
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="links">
+                        <h2>SITES</h2>
+                        <nav>
+                            <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">Mad Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="footer-image">
+                    <img src="/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>  
     </footer>
 </template>
 
@@ -80,6 +141,55 @@ footer {
             }
 
         }
+    }
+
+    .content-footer {
+        background-image: url('/footer-bg.jpg');
+        background-size: cover;
+
+        .container {
+            position: relative;
+            padding: 2rem 0;
+            @include space-between;
+
+            .footer-links {
+                display: flex;
+
+                .links {
+                    margin-right: 2rem;
+
+                    ul {
+                        list-style-type: none;
+
+                        li a {
+                            text-decoration: none;
+                            color: $grey;
+
+                            &:hover,
+                            &:active {
+                                color: $white
+                            }
+                        }
+                    }
+
+                    h2 {
+                        color: $white;
+                        padding: 1rem 0;
+                    }
+                }
+            }
+
+            .footer-image {
+                img {
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    transform: translateY(-15%);
+                }
+            }
+
+        }
+
     }
 }
 </style>
